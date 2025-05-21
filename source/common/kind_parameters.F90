952f029c8c5b54213a2ee2f3b1e18102c8f48e75
jrcking <jack.rc.king@gmail.com>
2025-05-21 08:28:36 +0100

    updated kind parameters

--- source/common/kind_parameters.F90
+++ source/common/kind_parameters.F90
@@ -8,8 +8,8 @@ module kind_parameters
   public :: rkind,ikind
 
 
-  integer ,parameter :: rkind =c_double !selected_real_kind(digits)
-  integer ,parameter :: ikind =c_int    !selected_int_kind(decades)
+  integer ,parameter :: rkind =selected_real_kind(15,307)
+  integer ,parameter :: ikind =selected_int_kind(9)
 !  integer, parameter :: rkind = selected_real_kind(33,4931)  !! QUAD PRECISION !!
 
 end module
